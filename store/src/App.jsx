471f6491addae9e676f788c33637179e447fea9d
Angel Esteban Patiño Salazar <angel.patino@samtel.local>
2023-11-20 16:21:11 -0500

    Creación de la estructura de carpetas del proyecto y primer componente de prueba

--- store/src/App.jsx
+++ store/src/App.jsx
@@ -1,7 +1,13 @@
-import './index.css'
+import './style/index.css'
+import Header from './components/header/Header.jsx'
 
 function App() {
-  return <h1 className='text-red-500'>Holaa</h1>
+  return(
+    <>
+      <h1 className='text-red-500'>Este es el App</h1>
+      <Header />
+    </>
+  )
 }
 
 export default App
